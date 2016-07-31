102e0e45df88eb6b4cd84977da72e81d1d622b8b
Katarina Mitchell <kat@uncorkedstudios.com>
2016-07-31 14:17:32 -0700

    Changing weather icon to black and white for ambient mode.

--- wear/src/main/java/com/katmitchell/wear/WeatherFace.java
+++ wear/src/main/java/com/katmitchell/wear/WeatherFace.java
@@ -28,6 +28,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.Typeface;
@@ -305,6 +307,16 @@ public class WeatherFace extends CanvasWatchFaceService implements DataApi.DataL
                 mAmbient = inAmbientMode;
                 if (mLowBitAmbient) {
                     mTimePaint.setAntiAlias(!inAmbientMode);
+                    mDatePaint.setAntiAlias(!inAmbientMode);
+                    mHighPaint.setAntiAlias(!inAmbientMode);
+                    mLowPaint.setAntiAlias(!inAmbientMode);
+
+                }
+                if (mAmbient) {
+                    mWeatherBitmap = toGrayscale(mWeatherBitmap);
+                } else {
+                    mWeatherBitmap = BitmapFactory
+                            .decodeResource(getResources(), R.drawable.ic_clear);
                 }
                 invalidate();
             }
@@ -312,6 +324,22 @@ public class WeatherFace extends CanvasWatchFaceService implements DataApi.DataL
             // Whether the timer should be running depends on whether we're visible (as well as
             // whether we're in ambient mode), so we may need to start or stop the timer.
             updateTimer();
+        }
+
+        public Bitmap toGrayscale(Bitmap bmpOriginal) {
+            int width, height;
+            height = bmpOriginal.getHeight();
+            width = bmpOriginal.getWidth();
+
+            Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            Canvas c = new Canvas(bmpGrayscale);
+            Paint paint = new Paint();
+            ColorMatrix cm = new ColorMatrix();
+            cm.setSaturation(0);
+            ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+            paint.setColorFilter(f);
+            c.drawBitmap(bmpOriginal, 0, 0, paint);
+            return bmpGrayscale;
         }
 
         @Override
